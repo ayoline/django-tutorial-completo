0a9765d54149b1148bb894038e99c4a953a84a1f
ayoline <ayo@outlook.com.br>
2022-05-13 20:34:33 -0300

    crestion of POST model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.forms import SlugField
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    STATUS = (
+        ('rascunho','Rascunho'),
+        ('publicado','Publicado'),
+    )
+    titulo = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    autor = models.ForeignKey(User,
+                            on_delete=models.CASCADE)
+    conteudo  = models.TextField()
+    publicado = models.DateTimeField(default=timezone.now)
+    criado    = models.DateTimeField(auto_now_add=True)
+    alterado  = models.DateTimeField(auto_now=True)
+    status    = models.CharField(max_length=10,
+                                choices=STATUS,
+                                default='rascunho')
+
+    class Meta:
+        ordering = ('-publicado',)
+
+    def __str__(self):
+        return self.titulo
+
 
 # Create your models here.
